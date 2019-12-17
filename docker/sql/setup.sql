d5c6039294adfd65e6597d261a7a33db7e005619
DeclanSL <49640187+DeclanSL@users.noreply.github.com>
2019-12-17 09:50:07 +0000

    Update setup.sql

--- docker/sql/setup.sql
+++ docker/sql/setup.sql
@@ -1,4 +1,12 @@
-CREATE TABLE testtable (
-    test1 int,
-    test2 int
+CREATE TABLE Transactions (
+    TransactionId bigint IDENTITY(1,1) PRIMARY KEY,
+    [Value] DECIMAL(19,4),
+	TransactionDate Datetime,
+	Summary varchar(255)
 );
+
+INSERT INTO Transactions ([Value], TransactionDate, Summary)
+VALUES
+(10.99, '2019-12-17', 'Pizza'),
+(30.56, '2019-12-16', 'Beers'),
+(55.00, '2019-12-15', 'Petrol')
