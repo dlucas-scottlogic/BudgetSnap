26a1386b2b4360386e794209cbb4c8289f084064
DeclanSL <49640187+DeclanSL@users.noreply.github.com>
2020-01-03 14:12:09 +0000

    Impliment numeric picker for New Transaction

--- BudgetSnap.Web/ClientApp/src/components/TransactionForm.jsx
+++ BudgetSnap.Web/ClientApp/src/components/TransactionForm.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { AddTransaction } from '../redux/actions/AddTransaction';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import NumericInput from 'react-numeric-input';
 
 class TransactionForm extends React.Component {
     constructor(props) {
@@ -13,15 +14,22 @@ class TransactionForm extends React.Component {
             summary: ""
         };
 
+        this.handleTextChange = this.handleTextChange.bind(this);
         this.handleValueChange = this.handleValueChange.bind(this);
         this.onSubmit = this.onSubmit.bind(this);
         this.handleDateChange = this.handleDateChange.bind(this);
     }
 
-    handleValueChange(e) {
+    handleTextChange(e) {
         this.setState({ [e.target.name]: e.target.value });
     }
-    
+
+    handleValueChange(valueAsNumber, _valueAsString , _e) {
+        this.setState({
+            value: valueAsNumber
+        });
+    }
+
     handleDateChange(date) {
         this.setState({
             startDate: date
@@ -42,29 +50,35 @@ class TransactionForm extends React.Component {
 
         // call action to save
         this.props.AddTransaction(post);
+
+        this.setState({
+            value: 0,
+            startDate: new Date(),
+            summary: ""
+        });
     }
 
-    render() {
-        return (
-            <div>
-                <h3>New Transaction</h3>
-                <form onSubmit={this.onSubmit} >
-                    <div>
-                        <label>Value (&#163;): </label>
-                        <input name="value" value={this.state.value} onChange={this.handleValueChange} type="text" />
-                        
-                        <label>Transaction Date: </label>
-                        <DatePicker selected={this.state.startDate} onChange={this.handleDateChange} />
+render() {
+    return (
+        <div>
+            <h3>New Transaction</h3>
+            <form onSubmit={this.onSubmit} >
+                <div>
+                    <label>Value (&#163;) : </label>
+                    <NumericInput name="value" value={this.state.value} onChange={this.handleValueChange} precision={2} step={1.0} snap />
 
-                        <label>Summary: </label>
-                        <input name="summary" value={this.state.summary} onChange={this.handleValueChange} type="text" />
+                    <label>Transaction Date : </label>
+                    <DatePicker selected={this.state.startDate} onChange={this.handleDateChange} />
 
-                        <button type="submit">Submit </ button>
-                    </div>
-                </form>
-            </div>
-        )
-    };
+                    <label>Summary : </label>
+                    <input name="summary" value={this.state.summary} onChange={this.handleTextChange} type="text" />
+
+                    <button type="submit">Submit </ button>
+                </div>
+            </form>
+        </div>
+    )
+};
 }
 
 export default connect(null, { AddTransaction })(TransactionForm)
