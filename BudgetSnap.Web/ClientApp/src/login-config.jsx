207ce3eaf33a4aa8455f303a40729f4002dee491
DeclanSL <49640187+DeclanSL@users.noreply.github.com>
2020-02-05 17:00:02 +0000

    addiitonal scopes

--- BudgetSnap.Web/ClientApp/src/login-config.jsx
+++ BudgetSnap.Web/ClientApp/src/login-config.jsx
@@ -2,6 +2,6 @@
     authority: "http://localhost:32782",
     client_id: "budgetsnap-frontend",
     response_type: "code",
-    scope: "openid profile",
+    scope: "openid profile email city_of_birth",
     redirect_uri: "http://localhost:2908/logincallback",
 };
